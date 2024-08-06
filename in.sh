bff40058f4f387a61c723b6bb6062179dab8fefc
itskiprotich <keeprawteachjapheth@gmail.com>
2024-08-06 14:34:59 +0300

    UPD: reporter sadrs

---
+++ in.sh
@@ -0,0 +1,5 @@
+git rm -r --cached bake_permissions.sh 
+git rm -r --cached bake_all_templates.sh
+git rm -r --cached bake_all_models.sh
+git rm -r --cached bake_all_controllers.sh
+git rm -r --cached bake_all_default_values.sh
